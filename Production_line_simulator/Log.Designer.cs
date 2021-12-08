9cfed9e5ce9f0e8be5dd91cc98d433a27d22d192
ojanczewska <olkamj@gmail.com>
2021-12-08 20:34:43 +0100

    files editing

---
+++ Production_line_simulator/Log.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Production_line_simulator
+{
+    partial class Log
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.login_textBox = new System.Windows.Forms.TextBox();
+            this.password_textBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(139, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(143, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Hasło:";
+            // 
+            // login_textBox
+            // 
+            this.login_textBox.Location = new System.Drawing.Point(226, 90);
+            this.login_textBox.Name = "login_textBox";
+            this.login_textBox.Size = new System.Drawing.Size(164, 26);
+            this.login_textBox.TabIndex = 2;
+            // 
+            // password_textBox
+            // 
+            this.password_textBox.Location = new System.Drawing.Point(226, 159);
+            this.password_textBox.Name = "password_textBox";
+            this.password_textBox.Size = new System.Drawing.Size(164, 26);
+            this.password_textBox.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(226, 233);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 36);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Zaloguj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Enter += new System.EventHandler(this.button1_Click);
+            // 
+            // Log
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(615, 303);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.password_textBox);
+            this.Controls.Add(this.login_textBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Log";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Logowanie";
+            this.Load += new System.EventHandler(this.Log_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.TextBox login_textBox;
+        public System.Windows.Forms.TextBox password_textBox;
+    }
+}
+
---
+++ Production_line_simulator/Log.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Production_line_simulator
+{
+    public partial class Log : Form
+    {
+
+        public static Log login = new Log();
+        public Log()
+        {
+            InitializeComponent();
+            login = this;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LogindbEntities context = new LogindbEntities();
+            if (login_textBox.Text != string.Empty || password_textBox.Text != string.Empty)
+            {
+                var user = context.AdminLogins.Where(a => a.UserName.Equals(login_textBox.Text)).FirstOrDefault();
+                if(user != null)
+                {
+                    if (user.Password.Equals(password_textBox.Text))
+                    {
+                        this.Hide();
+                        Form1 form = new Form1();
+                        form.Show();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Podany login i/lub hasło są nieprawidłowe.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Podany login i/lub hasło są nieprawidłowe.");
+                }
+            }
+        }
+
+        private void Log_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
